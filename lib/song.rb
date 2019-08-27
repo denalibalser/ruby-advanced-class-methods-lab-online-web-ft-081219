47cacc98657856092b692fc78640962f7a249292
Denali Balser <denali.balser167@gmail.com>
2019-08-27 14:09:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,10 +16,10 @@ class Song
     song
   end
 
-  def self.new_by_name(name)
+  def self.new_by_name(song_name_string)
     song = self.new
-    song.name = song
-    @@all << song
+    song.name = song_name_string
+    song
   end
 
 
